b713e284c043c41094c8fb8d79cb56939fa84c05
Witoo Harianto <witoo@outlook.com>
2014-01-16 10:21:58 +0700

    init

---
+++ src/Witooh/Uploader/UploaderServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Witooh\Uploader;
+
+use Illuminate\Support\ServiceProvider;
+
+class UploaderServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('witooh/uploader');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
